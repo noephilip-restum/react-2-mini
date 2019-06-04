4ccbb3388b249952cee36e475c40725646a8dc16
Aodhan Hayter <aodhan.hayter@gmail.com>
2019-06-04 14:14:18 -0600

    Setup gh pages

---
+++ precache-manifest.921b334f66109c34c9a694d82a73888e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ecfd9e33a80d3563991af096ce79f0",
+    "url": "/react-2-mini/index.html"
+  },
+  {
+    "revision": "bd0b941854ac5e62a8e6",
+    "url": "/react-2-mini/static/css/main.5f56f7e7.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/react-2-mini/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "bd0b941854ac5e62a8e6",
+    "url": "/react-2-mini/static/js/main.f6cb068d.chunk.js"
+  },
+  {
+    "revision": "413da1a15b8defd9e491",
+    "url": "/react-2-mini/static/js/runtime~main.69021ade.js"
+  }
+]);
