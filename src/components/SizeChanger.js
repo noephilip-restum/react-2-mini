48dac3c65b169506b4fe13b757c6712d485a1e91
noephilip-restum <noe.restum@boom.camp>
2019-11-12 14:35:11 +0800

    Initial Commit

--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from "react";
 
-export default function ColorChanger() {
+export default function ColorChanger(props) {
   return (
-    <select className="dropDownContainer">
+    <select
+      className="dropDownContainer"
+      onChange={e => props.update(parseInt(e.target.value))}
+      disabled={props.allowEdit === "false"}
+    >
       <option value="12px"> 12 </option>
       <option value="13px"> 13 </option>
       <option value="14px"> 14 </option>
--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class TextContainer extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      text: '',
+      text: ""
     };
+
+    this.updateText = this.updateText.bind(this);
   }
 
   updateText(e) {
     this.setState({
-      text: e.target.value,
+      text: e.target.value
     });
   }
 
@@ -18,7 +20,11 @@ export default class TextContainer extends Component {
     return (
       <div className="textContainer">
         <textarea
-          // style
+          style={{
+            color: this.props.fontColor,
+            fontFamily: this.props.fontFamily,
+            fontSize: this.props.fontSize
+          }}
           onChange={this.updateText}
           value={this.state.text}
           placeholder="Start typing your thoughts here!"
